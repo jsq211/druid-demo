7303985894495ed589280ee3f4d358a595eb3a21
jsq211 <2369167320@qq.com>
2020-04-30 17:46:04 +0800

    异步测试

--- src/main/java/com/jsq/demo/common/utils/Test.java
+++ src/main/java/com/jsq/demo/common/utils/Test.java
@@ -1,7 +1,8 @@
 package com.jsq.demo.common.utils;
 
 public class Test {
+
     public static void pring(){
-        System.out.println("打印  abcd");
+        System.out.println("异步打印测试");
     }
 }
