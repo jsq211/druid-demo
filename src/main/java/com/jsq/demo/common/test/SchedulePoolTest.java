45ca893b4bfc2fa96e6d5fb48ddf6d504228c715
jsq211 <2369167320@qq.com>
2020-05-21 10:14:02 +0800

    线程池测试

--- src/main/java/com/jsq/demo/common/test/SchedulePoolTest.java
+++ src/main/java/com/jsq/demo/common/test/SchedulePoolTest.java
@@ -2,12 +2,13 @@ package com.jsq.demo.common.test;
 
 
 import com.jsq.demo.common.utils.Test;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SchedulePoolTest {
-    private static ScheduledExecutorService  scheduledExecutorService = new ScheduledThreadPoolExecutor(2);
+//    private static ScheduledExecutorService  scheduledExecutorService = new ScheduledThreadPoolExecutor(2);
 
     public static void main(String[] args) {
         AtomicInteger atomicInteger = new AtomicInteger(0);
@@ -24,28 +25,48 @@ public class SchedulePoolTest {
 //                Test.pring();
 //                System.out.println("当前线程："+Thread.currentThread() + atomicInteger.getAndIncrement());
 //            }
-//        },0, 1, TimeUnit.SECONDS);
+//        },0, 2, TimeUnit.SECONDS);
 //
 //        scheduledExecutorService.shutdown();
-        ExecutorService executorService = new ThreadPoolExecutor(1,1,2L,TimeUnit.SECONDS,new LinkedBlockingQueue<>());
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(1,1,2L,TimeUnit.SECONDS,new LinkedBlockingQueue<>());
         Callable callable = new Callable() {
             @Override
             public Object call() throws Exception {
                 Test.pring();
                 System.out.println(Thread.currentThread() + " " +atomicInteger.getAndIncrement());
-                Thread.sleep(2000);
+                Thread.sleep(3000);
                 return null;
             }
         };
-        Future future = executorService.submit(callable);
-        try {
-            future.get(2,TimeUnit.SECONDS);
-        } catch (Exception e) {
-            System.out.println(e);
-            System.out.println(Thread.currentThread());
-            Thread.currentThread().interrupt();
-            System.out.println("结束");
+        for (int i = 0; i < 100000; i++) {
+
+                Future future = executorService.submit(callable);
+                try {
+                    future.get(2,TimeUnit.SECONDS);
+                } catch (Exception e) {
+                    System.out.println(e);
+                    System.out.println(Thread.currentThread());
+                    Thread.currentThread().interrupt();
+                    System.out.println("结束");
+                }
+
         }
+
+
+//        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+//        threadPoolTaskExecutor.setCorePoolSize(1);
+//        threadPoolTaskExecutor.setMaxPoolSize(1);
+//        threadPoolTaskExecutor.setKeepAliveSeconds(2);
+//        threadPoolTaskExecutor.initialize();
+//        Future future = threadPoolTaskExecutor.submit(callable);
+//        try {
+//            future.get(2,TimeUnit.SECONDS);
+//        } catch (Exception e) {
+//            System.out.println(e);
+//            System.out.println(Thread.currentThread());
+//            Thread.currentThread().interrupt();
+//            System.out.println("结束");
+//        }
     }
 
 }
